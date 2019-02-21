6dadb2806f74a235a771eef365647ada07cb5e6c
Alex Anderson <a.anderson@ncl.ac.uk>
2019-02-21 13:49:21 +0000

    Set primary app color

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Color(0xFF9DAF59),
+      ),
       home: Scaffold(
         appBar: AppBar(
           title: Text('Project App'),
