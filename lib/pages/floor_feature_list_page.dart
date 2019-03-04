c2525afc29a1e85fef7ea442283f40b39842ad9a
Alex Anderson <alex@alwian.com>
2019-03-04 16:38:58 +0000

    Added and example of a floor feature

--- lib/pages/floor_feature_list_page.dart
+++ lib/pages/floor_feature_list_page.dart
@@ -8,8 +8,35 @@ class FloorFeatureListPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[300],
       appBar: AppBar(
         title: Text('Floor ' + _floor.toString()),
+      ),
+      body: ListView(
+        children: <Widget>[
+          Card(
+            margin: EdgeInsets.all(16.0),
+            shape: BeveledRectangleBorder(),
+            color: Color(0xFF313131),
+            child: Stack(
+              children: <Widget>[
+                Image.asset('assets/images/lecture_theatre.jpg'),
+                Container(
+                  margin: EdgeInsets.all(20.0),
+                  padding: EdgeInsets.only(left: 10.0, top: 7.5, right: 20.0, bottom: 7.5),
+                  color: Color(0xFF313131),
+                  child: Text(
+                    'Lecture Theatre',
+                    style: TextStyle(
+                      color: Theme.of(context).primaryColor,
+                      fontSize: 20.0
+                    ),
+                  ),
+                )
+              ],
+            )
+          )
+        ],
       ),
     );
   }
