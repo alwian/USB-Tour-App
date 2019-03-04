e1d7fa2ddbf3fbe30e56346c0d6463d9fc3c1e91
Alex Anderson <alex@alwian.com>
2019-03-04 14:41:34 +0000

    Added background color

--- lib/fragments/explore_a_floor_fragment.dart
+++ lib/fragments/explore_a_floor_fragment.dart
@@ -3,123 +3,128 @@ import 'package:flutter/material.dart';
 class ExploreAFloorFragment extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Image.asset('assets/images/usb.jpg'),
-        Expanded(
-          child: Center(
-            child: Container(
-              width: (MediaQuery.of(context).size.width / 3) * 2,
-              child: GridView.count(
-                crossAxisCount: 2,
-                physics: ScrollPhysics(),
-                shrinkWrap: true,
-                padding: EdgeInsets.all(20.0),
-                mainAxisSpacing: 20.0,
-                crossAxisSpacing: 20.0,
-                children: <Widget>[
-                  RawMaterialButton(
-                    shape: CircleBorder(),
-                    fillColor: Color(0xFFDC817E),
-                    onPressed: () => {},
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Text(
-                          '1',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 30.0
+    return DecoratedBox(
+      decoration: BoxDecoration(
+        color: Color(0xFF333333)
+      ),
+      child: Column(
+        children: <Widget>[
+          Image.asset('assets/images/usb.jpg'),
+          Expanded(
+            child: Center(
+              child: Container(
+                width: (MediaQuery.of(context).size.width / 3) * 2,
+                child: GridView.count(
+                  crossAxisCount: 2,
+                  physics: ScrollPhysics(),
+                  shrinkWrap: true,
+                  padding: EdgeInsets.all(20.0),
+                  mainAxisSpacing: 20.0,
+                  crossAxisSpacing: 20.0,
+                  children: <Widget>[
+                    RawMaterialButton(
+                      shape: CircleBorder(),
+                      fillColor: Color(0xFFDC817E),
+                      onPressed: () => {},
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Text(
+                            '1',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 30.0
+                            ),
                           ),
-                        ),
-                        Text(
-                          'Floor',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 15.0
+                          Text(
+                            'Floor',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 15.0
+                            ),
                           ),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
-                  ),
-                  RawMaterialButton(
-                    shape: CircleBorder(),
-                    fillColor: Color(0xFFE59D62),
-                    onPressed: () => {},
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Text(
-                          '2',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 30.0
+                    RawMaterialButton(
+                      shape: CircleBorder(),
+                      fillColor: Color(0xFFE59D62),
+                      onPressed: () => {},
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Text(
+                            '2',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 30.0
+                            ),
                           ),
-                        ),
-                        Text(
-                          'Floor',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 15.0
+                          Text(
+                            'Floor',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 15.0
+                            ),
                           ),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
-                  ),
-                  RawMaterialButton(
-                    shape: CircleBorder(),
-                    fillColor: Color(0xFFE2DE83),
-                    onPressed: () => {},
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Text(
-                          '3',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 30.0
+                    RawMaterialButton(
+                      shape: CircleBorder(),
+                      fillColor: Color(0xFFE2DE83),
+                      onPressed: () => {},
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Text(
+                            '3',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 30.0
+                            ),
                           ),
-                        ),
-                        Text(
-                          'Floor',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 15.0
+                          Text(
+                            'Floor',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 15.0
+                            ),
                           ),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
-                  ),
-                  RawMaterialButton(
-                    shape: CircleBorder(),
-                    fillColor: Color(0xFFB4D47F),
-                    onPressed: () => {},
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Text(
-                          '4',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 30.0
+                    RawMaterialButton(
+                      shape: CircleBorder(),
+                      fillColor: Color(0xFFB4D47F),
+                      onPressed: () => {},
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Text(
+                            '4',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 30.0
+                            ),
                           ),
-                        ),
-                        Text(
-                          'Floor',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 15.0
+                          Text(
+                            'Floor',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 15.0
+                            ),
                           ),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
-                  ),
-                ]
+                  ]
+                ),
               ),
             ),
-          ),
-        )
-      ],
+          )
+        ],
+      ),
     );
   }
 }
