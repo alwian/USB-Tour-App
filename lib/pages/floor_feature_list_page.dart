17cef05be78a90e9a3651de2fb2516c0945d8fe6
Alex Anderson <alex@alwian.com>
2019-03-17 18:04:07 +0000

    Added a _FloorFeature class

--- lib/pages/floor_feature_list_page.dart
+++ lib/pages/floor_feature_list_page.dart
@@ -1,9 +1,50 @@
 import 'package:flutter/material.dart';
 
+class _FloorFeature {
+  String name;
+  String image;
+
+  _FloorFeature(this.name, this.image);
+}
+
 class FloorFeatureListPage extends StatelessWidget {
   final int _floor;
+  final _features = [
+    _FloorFeature('Lecture Theatre', 'assets/images/lecture_theatre.jpg'),
+    _FloorFeature('Lecture Theatre', 'assets/images/lecture_theatre.jpg'),
+    _FloorFeature('Lecture Theatre', 'assets/images/lecture_theatre.jpg'),
+    _FloorFeature('Lecture Theatre', 'assets/images/lecture_theatre.jpg'),
+    _FloorFeature('Lecture Theatre', 'assets/images/lecture_theatre.jpg'),
+    _FloorFeature('Lecture Theatre', 'assets/images/lecture_theatre.jpg'),
+  ];
 
   FloorFeatureListPage(this._floor);
+
+  _buildFeatureList() {
+    var list = <Widget>[];
+    for(int i = 0; i < _features.length; i++) {
+      list.add(
+        Stack(
+          children: <Widget>[
+            Image.asset(_features[i].image),
+            Container(
+              margin: EdgeInsets.all(20.0),
+              padding: EdgeInsets.only(left: 10.0, top: 7.5, right: 20.0, bottom: 7.5),
+              color: Color(0xFFB3B3B3),
+              child: Text(
+                _features[i].name,
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20.0
+                ),
+              ),
+            )
+          ],
+        )
+      );
+    }
+    return list;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,25 +54,7 @@ class FloorFeatureListPage extends StatelessWidget {
         title: Text('Floor ' + _floor.toString()),
       ),
       body: ListView(
-        children: <Widget>[
-          Stack(
-            children: <Widget>[
-              Image.asset('assets/images/lecture_theatre.jpg'),
-              Container(
-                margin: EdgeInsets.all(20.0),
-                padding: EdgeInsets.only(left: 10.0, top: 7.5, right: 20.0, bottom: 7.5),
-                color: Color(0xFFB3B3B3),
-                child: Text(
-                  'Lecture Theatre',
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20.0
-                  ),
-                ),
-              )
-            ],
-          )
-        ],
+        children: _buildFeatureList()
       ),
     );
   }
