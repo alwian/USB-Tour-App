b6cc518235acd981118cb0e47e6f564676a400f1
Alex Anderson <alex@alwian.com>
2019-03-17 23:00:32 +0000

    Added the ability to display list of features

--- lib/pages/room_feature_list_page.dart
+++ lib/pages/room_feature_list_page.dart
@@ -1,9 +1,25 @@
 import 'package:flutter/material.dart';
 
+class _RoomFeature {
+  String image;
+  String description;
+
+  _RoomFeature(this.image, this.description);
+}
+
 class RoomFeatureListPage extends StatelessWidget {
   String roomName;
 
   RoomFeatureListPage(this.roomName);
+
+  final _features = [
+    _RoomFeature('assets/images/flat_floor.jpg', 'Placeholder text...'),
+    _RoomFeature('assets/images/flat_floor.jpg', 'Placeholder text...'),
+    _RoomFeature('assets/images/flat_floor.jpg', 'Placeholder text...'),
+    _RoomFeature('assets/images/flat_floor.jpg', 'Placeholder text...'),
+    _RoomFeature('assets/images/flat_floor.jpg', 'Placeholder text...'),
+    _RoomFeature('assets/images/flat_floor.jpg', 'Placeholder text...'),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -11,27 +27,32 @@ class RoomFeatureListPage extends StatelessWidget {
       appBar: AppBar(
         title: Text(roomName),
       ),
-      body: Card(
-        margin: EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Image.asset('assets/images/flat_floor.jpg'),
-            Container(
-              margin: EdgeInsets.only(top: 10.0),
-              color: Color(0xFFE0E0E0),
-              child: Row(
-                children: <Widget>[
-                  Padding(
-                    child: Text('Placeholder text...'),
-                    padding: EdgeInsets.only(left: 5.0, top: 5.0, right: 10.0, bottom: 10.0),
-                  )
-                ],
-              ),
-            )
-          ],
-        ),
+      body: ListView.builder(
+        itemCount: _features.length,
+        itemBuilder: (context, index) {
+          return  Card(
+            margin: EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Image.asset(_features[index].image),
+                Container(
+                  margin: EdgeInsets.only(top: 10.0),
+                  color: Color(0xFFE0E0E0),
+                  child: Row(
+                    children: <Widget>[
+                      Padding(
+                        child: Text(_features[index].description),
+                        padding: EdgeInsets.only(left: 5.0, top: 5.0, right: 10.0, bottom: 10.0),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          );
+        }
       )
     );
   }
