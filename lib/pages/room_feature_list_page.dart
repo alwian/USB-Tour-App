1318c81901d66c30a4364c228e6182c5dcc14382
Alex Anderson <alex@alwian.com>
2019-03-17 22:54:12 +0000

    Added example of a room feature

--- lib/pages/room_feature_list_page.dart
+++ lib/pages/room_feature_list_page.dart
@@ -10,6 +10,28 @@ class RoomFeatureListPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text(roomName),
+      ),
+      body: Card(
+        margin: EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Image.asset('assets/images/flat_floor.jpg'),
+            Container(
+              margin: EdgeInsets.only(top: 10.0),
+              color: Color(0xFFE0E0E0),
+              child: Row(
+                children: <Widget>[
+                  Padding(
+                    child: Text('Placeholder text...'),
+                    padding: EdgeInsets.only(left: 5.0, top: 5.0, right: 10.0, bottom: 10.0),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
       )
     );
   }
