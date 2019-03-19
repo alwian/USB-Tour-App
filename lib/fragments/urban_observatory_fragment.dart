a404d6f36af78c0cbc77bafe9d773820b615f6d1
Alex Anderson <alex@alwian.com>
2019-03-19 15:58:38 +0000

    Added tabs for each section

--- lib/fragments/urban_observatory_fragment.dart
+++ lib/fragments/urban_observatory_fragment.dart
@@ -3,10 +3,18 @@ import 'package:flutter/material.dart';
 class UrbanObservatoryFragment extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Text('Urban Observatory fragment')
-      ],
+    return DefaultTabController(
+      length: 2,
+      child: Column(
+        children: <Widget>[
+          TabBar(
+            tabs: <Widget>[
+              Tab(icon: Icon(Icons.info)),
+              Tab(icon: Icon(Icons.search)),
+            ],
+          )
+        ],
+      ),
     );
   }
 }
