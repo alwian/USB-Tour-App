039e046bb72e1fca2de37af9a288a7eb5fd27079
Connor Harris <c.harris99@hotmail.co.uk>
2019-03-19 22:14:06 +0000

    Added placeholder image and text

--- lib/fragments/find_a_room_fragment.dart
+++ lib/fragments/find_a_room_fragment.dart
@@ -1,12 +1,36 @@
 import 'package:flutter/material.dart';
 
 class FindARoomFragment extends StatelessWidget {
+
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Text('Find a room fragment')
-      ],
+    return DecoratedBox(
+      decoration: BoxDecoration(
+          color: Color(0xFFFFFF)
+      ),
+      child: Column(
+        children: <Widget>[
+          Stack(
+            children: <Widget>[
+              Image.asset('assets/images/lecture_theatre.jpg'),
+              Container(
+                margin: EdgeInsets.all(20.0),
+                padding: EdgeInsets.only(left: 10.0, top: 10.0, right: 10.0, bottom: 10.0),
+                color: Color(0xFF313131),
+                child: Text(
+                  'Search for a room!',
+                  //@todo center text in stack
+                  style: TextStyle(
+                      color: Theme.of(context).primaryColor,
+                      fontSize: 20.0,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
