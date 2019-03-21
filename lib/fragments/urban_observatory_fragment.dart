959cbeff0e9f4485be378083f282b69c49d58ac9
Alex Anderson <alex@alwian.com>
2019-03-21 13:02:25 +0000

    Added a view for each tab

--- lib/fragments/urban_observatory_fragment.dart
+++ lib/fragments/urban_observatory_fragment.dart
@@ -5,15 +5,27 @@ class UrbanObservatoryFragment extends StatelessWidget {
   Widget build(BuildContext context) {
     return DefaultTabController(
       length: 2,
-      child: Column(
-        children: <Widget>[
-          TabBar(
-            tabs: <Widget>[
-              Tab(icon: Icon(Icons.info)),
-              Tab(icon: Icon(Icons.search)),
-            ],
-          )
-        ],
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: Column(
+          children: <Widget>[
+            TabBar(
+              tabs: <Widget>[
+                Tab(icon: Icon(Icons.info)),
+                Tab(icon: Icon(Icons.search)),
+              ],
+            ),
+            Expanded(
+              child: TabBarView(
+                children: <Widget>[
+                  Text('Info'),
+                  Text('Search'),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
