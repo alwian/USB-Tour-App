7a720c0ff11260f5ac68c19376056abd2821fe02
Mason Powell <mj.powell@hotmail.com>
2019-03-26 20:49:12 +0000

    Modified nav to match input source with node stored in graph
    Removed print statements as well

--- lib/algorithm/navigation.dart
+++ lib/algorithm/navigation.dart
@@ -8,30 +8,33 @@ import 'dart:collection';
 
 class Navigation {
   static Graph calculateShortestPathFromSource(Graph graph, Node source) {
-    print("cSPFS running");
     //Returns the graph with the shortestPath value and distance values set relative to the source
-    source.distance = 0;
+    Node nSource;
+    for (Node u in graph.nodes) {
+      if (u.name == source.name) {
+        nSource = u;
+        break;
+      }
+    }
+    nSource.distance = 0;
 
     Set<Node> settledNodes =
         {}; //Settled nodes are nodes that we have finished looking at
     Set<Node> unsettledNodes =
         {}; //Unsettled nodes are the nodes that are connected to the current node
 
-    unsettledNodes.add(source);
+    unsettledNodes.add(nSource);
 
     while (unsettledNodes.length != 0) {
       //Unsettled nodes should always have at least one node in it until all nodes have been looked at
       Node currentNode = getLowestDistanceNode(unsettledNodes);
-      print("currentNode: " + currentNode.name);
       unsettledNodes.remove(currentNode);
-      print("just before the loop");
-print(currentNode.adjacentNodes.entries);
+
       for (MapEntry<Node, int> adjacencyPair
           in currentNode.adjacentNodes.entries) {
-        print("running the loop");
+
         //AdjacencyPair stores the key-value mapping
         Node adjacentNode = adjacencyPair.key;
-        print(adjacentNode);
         int edgeWeight = adjacencyPair.value; //Same as distance
         if (!settledNodes.contains(adjacentNode)) {
           calculateMinimumDistance(adjacentNode, edgeWeight, currentNode);
@@ -68,8 +71,8 @@ print(currentNode.adjacentNodes.entries);
 
       shortestPath.add(sourceNode);
       evaluationNode.shortestPath = shortestPath;
-      print("Eval node: " + evaluationNode.name);
-      print(evaluationNode.shortestPath.isNotEmpty);
+
+
     }
   }
 
@@ -81,7 +84,7 @@ print(currentNode.adjacentNodes.entries);
 
     for (Node w in graph.nodes) {
       if (w.name == target.name) {
-        print("target found");
+
         nTarget = w;
         break;
       }
@@ -89,22 +92,19 @@ print(currentNode.adjacentNodes.entries);
 
     for (Node v in graph.nodes) {
       if (v.name == source.name) {
-        print("source found");
+
         nSource = v;
         break;
       }
     }
 
-    print(nTarget.shortestPath.isNotEmpty);
     if (nTarget == nSource || nTarget.shortestPath.isNotEmpty) {
-      print("running1");
+
       //Only if the node is reachable should we do something
       while (nTarget != null) {
-        print("running2");
         path.addLast(nTarget); //AddLast and removeLast let the Queue represent a stack
 
         if (nTarget.shortestPath.isNotEmpty) {
-          print("running3");
           nTarget = nTarget.shortestPath.last;
           /* ShortestPath has the path of nodes to the source,
            * with the last node also being the closest
