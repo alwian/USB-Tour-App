64bf5c18cc5c356025b6880d922c2b5a86d2d239
JamWolf <b6037915@newcastle.ac.uk>
2019-03-28 13:33:47 +0000

    Replaced placeholder text for Opening times and Contact

--- lib/fragments/building_information_fragment.dart
+++ lib/fragments/building_information_fragment.dart
@@ -25,16 +25,54 @@ class BuildingInformationFragment extends StatelessWidget {
                 child: TabBarView(
                   children: <Widget>[
                     Container(
+                      padding: const EdgeInsets.only(left: 20),
                       color: Colors.grey[400],
-                      child: Text('About text'),
+                      child: ListView(
+                        children: <Widget>[
+                          Text(
+                            '\n\nAbout title',
+                            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                          ),
+                          Text('\nAbout text'),
+                        ],
+                      ),
                     ),
                     Container(
+                      padding: const EdgeInsets.only(left: 20),
                       color: Colors.grey[400],
-                      child: Text('Opening times text'),
+                      child: ListView(
+                        children: <Widget>[
+                          Text(
+                            '\n\nReception opening hours',
+                            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                          ),
+                          Text('\nMonday to Friday, 9:00 - 17:00'),
+                          Text(
+                            '\n\nBuilding opening hours',
+                            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                          ),
+                          Text('\nMonday to Friday, 9:00 - 22:00'),
+                          Text('Saturday and Sunday, 9:00 - 21:00'),
+                        ],
+                      ),
                     ),
                     Container(
+                      padding: const EdgeInsets.only(left: 20),
                       color: Colors.grey[400],
-                      child: Text('Contact text'),
+                      child: ListView(
+                        children: <Widget>[
+                          Text(
+                            '\n\nAddress',
+                            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                          ),
+                          Text('\nSchool of Computing\nUrban Sciences Building\nNewcastle University\n1 Science Square\nNewcastle Helix\nNewcastle upon Tyne\nNE4 5TG'),
+                          Text(
+                            '\n\nEmail and phone',
+                            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                          ),
+                          Text('\nTelephone: +44 (0)191 208 7972\nFax: +44 (0)191 208 8232\nGeneral email: computing@ncl.ac.uk\nStudent admissions email: cs.admissions@newcastle.ac.uk'),
+                        ],
+                      ),
                     ),
                   ],
                 ),
