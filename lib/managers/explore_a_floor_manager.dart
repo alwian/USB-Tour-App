73eeddf5c5c0490aa319956b5568aba7d5145835
Alex Anderson <alex@alwian.com>
2019-04-03 18:28:24 +0100

    Added method for collecting room features from a database

--- lib/managers/explore_a_floor_manager.dart
+++ lib/managers/explore_a_floor_manager.dart
@@ -8,6 +8,13 @@ class Room {
   Room(this.name, this.image);
 }
 
+class RoomFeature {
+  String description;
+  String image;
+
+  RoomFeature(this.description, this.image);
+}
+
 class ExploreAFloorManager {
   static Future<List<Room>> getRooms(floor) async {
     String dbsPath = await getDatabasesPath();
@@ -27,5 +34,25 @@ class ExploreAFloorManager {
       );
     }
     return rooms;
+  }
+
+  static Future<List<RoomFeature>> getRoomFeatures(room) async {
+    String dbsPath = await getDatabasesPath();
+    String dbPath = join(dbsPath, "data.db");
+
+    Database database = await openDatabase(dbPath);
+    List<Map<String, dynamic>> queryResults;
+    await database.transaction((txn) async {
+      String query = "SELECT * FROM room_features WHERE room='$room'";
+      queryResults = await txn.rawQuery(query);
+    });
+
+    List<RoomFeature> features = [];
+    for (Map<String, dynamic> m in queryResults) {
+      features.add(
+          RoomFeature(m['description'], m['image'])
+      );
+    }
+    return features;
   }
 }
