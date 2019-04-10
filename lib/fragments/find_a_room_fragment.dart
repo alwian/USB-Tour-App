e015621157e248da991a65b474f26c81c3e1b458
Connor Harris <c.harris99@hotmail.co.uk>
2019-04-10 15:14:53 +0100

    Resolved pixel overflow issue

--- lib/fragments/find_a_room_fragment.dart
+++ lib/fragments/find_a_room_fragment.dart
@@ -1,48 +1,65 @@
 import 'package:flutter/material.dart';
+import 'package:csc2022_app/pages/search_results_page.dart';
 
 class FindARoomFragment extends StatelessWidget {
 
 
   @override
   Widget build(BuildContext context) {
-    return DecoratedBox(
-      decoration: BoxDecoration(
-          color: Color(0xFFFFFF)
-      ),
+    return new SingleChildScrollView(
       child: Column(
         children: <Widget>[
-          Stack(
-            children: <Widget>[
-              Image.asset('assets/images/lecture_theatre.jpg'),
-              Container(
-                margin: EdgeInsets.all(20.0),
-                padding: EdgeInsets.only(left: 10.0, top: 10.0, right: 10.0, bottom: 10.0),
-                color: Color(0xFF313131),
-                child: Text(
-                  'Search for a room!',
-                  //@todo center text in stack
-                  style: TextStyle(
-                      color: Theme.of(context).primaryColor,
-                      fontSize: 20.0,
+          DecoratedBox(
+            decoration: BoxDecoration( color: Color(0xFFFFFF)),
+            child: Column(
+              children: <Widget>[
+                Stack(
+                  children: <Widget>[
+                    Image.asset('assets/images/lecture_theatre.jpg'),
+                    Container(
+                      margin: EdgeInsets.all(50.0),
+                      padding: EdgeInsets.only(left: 10.0, top: 10.0, right: 10.0, bottom: 10.0),
+                      color: Color(0xFF313131),
+                      child: Center(child: new Text(
+                        'Search for a room!',
+                        //@todo center text in stack
+                        style: TextStyle(
+                          color: Theme.of(context).primaryColor,
+                          fontSize: 20.0,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),),
+
+                    ),
+                  ],
+                ),
+
+                //@todo Improve styling - https://medium.com/flutter-community/breaking-layouts-in-rows-and-columns-in-flutter-8ea1ce4c1316
+                //@todo Hard code suggestion dropdowns
+                //@todo add new search page
+
+                TextFormField(
+                  autofocus: false,
+                  decoration: InputDecoration(
+                      labelText: 'Enter Destination'
                   ),
                 ),
-              ),
-            ],
-          ),
-          //@todo Fix button pixel overflow issue
-          //@todo Improve styling - https://medium.com/flutter-community/breaking-layouts-in-rows-and-columns-in-flutter-8ea1ce4c1316
-          //@todo Hard code suggestion dropdowns
-          //@todo add new search page
-          TextFormField(
-            autofocus: false,
-            decoration: InputDecoration(
-              labelText: 'Enter Destination'
-            ),
-          ),
-          TextFormField(
-            autofocus: false,
-            decoration: InputDecoration(
-                labelText: 'Enter Current Location'
+                TextFormField(
+                  autofocus: false,
+                  decoration: InputDecoration(
+                      labelText: 'Enter Current Location'
+                  ),
+                ),
+                RaisedButton(
+                    child: new Text('Find your room'),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => SearchResultsPage()),
+                      );
+                    }
+                ),
+              ],
             ),
           ),
         ],
