a83f5c74e47d0965c4669fcbc665beccbbedd51f
Connor Harris <c.harris99@hotmail.co.uk>
2019-04-17 01:12:44 +0100

    Added getRoomSuggestions method to return a list of rooms that are like a given pattern

--- lib/managers/find_a_room_manager.dart
+++ lib/managers/find_a_room_manager.dart
@@ -24,11 +24,26 @@ class FindARoomManager {
   }
 
   /// Returns all [Room]s where name exists in the database.
-  ///
   static Future<List<Room>> getRoom(String room) async {
     // Execute query, return rows
     List<Map<String, dynamic>> queryResults = await DatabaseHelper.query(
         "SELECT * FROM rooms WHERE room='$room'"
+    );
+    List<Room> rooms = <Room>[];
+    // Create a [Room] for a ll rows returned from the DB query.
+    for (Map<String, dynamic> m in queryResults) {
+      rooms.add(
+          Room(m['name'], m['image'])
+      );
+    }
+    return rooms;
+  }
+
+  /// Returns all [Room]s where name is similar to a name in the database.
+  static Future<List<Room>> getRoomSuggestions(String pattern) async {
+    // Execute query, return rows
+    List<Map<String, dynamic>> queryResults = await DatabaseHelper.query(
+        "SELECT * FROM rooms WHERE room LIKE '$pattern'"
     );
     List<Room> rooms = <Room>[];
     // Create a [Room] for a ll rows returned from the DB query.
