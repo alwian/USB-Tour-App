eeddef29fe6170055e0f8dc09d3c67a5c239367c
Connor Harris <c.harris99@hotmail.co.uk>
2019-04-17 01:44:19 +0100

    Added dropdown autocomplete to first form field. For documentation see https://pub.dartlang.org/packages/flutter_typeahead#-readme-tab-

--- lib/fragments/find_a_room_fragment.dart
+++ lib/fragments/find_a_room_fragment.dart
@@ -1,8 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:csc2022_app/pages/search_results_page.dart';
 import 'package:flutter_typeahead/flutter_typeahead.dart';
+import 'package:csc2022_app/managers/find_a_room_manager.dart';
 
 class FindARoomFragment extends StatelessWidget {
+
+  ///Form keys and textEditingController
+  ///For more info on TextEditingController, see https://pub.dartlang.org/packages/flutter_typeahead#-readme-tab-
+  final GlobalKey<FormState> _findARoomFormKey = GlobalKey<FormState>();
+  final TextEditingController _typeAheadController = TextEditingController();
+
+  ///Strings to store results from the form text fields
+  String _firstSelectedRoom;
+
+  ///Load all [Room]s from database into List
 
 
   @override
@@ -24,7 +35,7 @@ class FindARoomFragment extends StatelessWidget {
                       color: Color(0xFF313131),
                       child: Text(
                         'Search for a room!',
-                        //@todo center text in stack
+                        //@todo Add center wrapper back and chenage image + remove decorated box and text colour
                         style: TextStyle(
                           color: Theme.of(context).primaryColor,
                           fontSize: 27.0,
@@ -40,51 +51,105 @@ class FindARoomFragment extends StatelessWidget {
                 //@todo Hard code suggestion dropdowns
                 //@todo add new search page
 
-                Padding(
-                  padding: EdgeInsets.all(15.0),
-                  child: TextFormField(
-                    autofocus: false,
-                    decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: 'Enter Destination'
-                    ),
-                  ),
-                ),
-
-                Padding(
-                  padding: EdgeInsets.all(15.0),
-                  child: TextFormField(
-                    autofocus: false,
-                    decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: 'Enter Current Location'
-                    ),
-                  ),
-                ),
-
-                Padding(
-                  padding: EdgeInsets.all(15.0),
-                  child: new MaterialButton(
-                    height: 75.0,
-                    minWidth: 125.0,
-                    color: Colors.black54,
-                    textColor: Colors.white,
-
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => SearchResultsPage()),
-                      );
-                    },
-                    splashColor: Theme.of(context).primaryColor,
-                    child: Row(
+                ///Form to submit start and end points for the route finding algorithm /algorithm/
+                Form(
+                  key: this._findARoomFormKey,
+                  child: Padding(
+                    padding: EdgeInsets.all(15.0),
+                    child: Column(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: <Widget>[
-                        Text(
-                          'Find your room now!',
-                          style: TextStyle(
-                            fontSize: 25.0,
-                            color: Colors.white,
+                        Padding(
+                          padding: EdgeInsets.all(15.0),
+                          child: TypeAheadFormField(
+                            textFieldConfiguration: TextFieldConfiguration(
+                              controller: this._typeAheadController,
+                              autofocus: false,
+                              decoration: InputDecoration(
+                                border: OutlineInputBorder(),
+                                labelText: 'Enter Destination',
+                              )
+                            ),
+
+                            ///Method to get suggestions to populate dropdown
+                            suggestionsCallback: (pattern) {
+                              //Call method to find rooms with similar name
+                              return FindARoomManager.getRoomSuggestions(pattern);
+                            }, //suggestionsCallback
+
+                            ///Build Dropdown menu
+                            itemBuilder: (context, suggestion) {
+                              return ListTile(
+                                title: Text(suggestion),
+                              );
+                            }, //itemBuilder
+
+                            ///Create loading animation
+                            transitionBuilder: (context, suggestionsBox, animationController) =>
+                              FadeTransition(
+                                child: suggestionsBox,
+                                opacity: CurvedAnimation(
+                                parent: animationController,
+                                curve: Curves.fastOutSlowIn
+                              ),
+                            ),
+
+                            ///Set TextField value to suggestion
+                            onSuggestionSelected: (suggestion) {
+                              this._typeAheadController.text = suggestion;
+                            },
+
+                            ///Validation method @todo add more comprehensive checks
+                            validator: (value) {
+                              if (value.isEmpty) {
+                                return 'Please select a room';
+                              }
+                            },
+
+                            ///Set string to selected value
+                            onSaved: (value) => this._firstSelectedRoom = value,
+                          ),
+                        ),
+
+                        Padding(
+                          padding: EdgeInsets.all(15.0),
+                          child: TextFormField(
+                            autofocus: false,
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(),
+                              labelText: 'Enter Current Location'
+                            ),
+                          ),
+                        ),
+
+                        Padding(
+                          padding: EdgeInsets.all(15.0),
+                          child: MaterialButton(
+                            height: 75.0,
+                            minWidth: 125.0,
+                            color: Colors.black54,
+                            textColor: Colors.white,
+
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(builder: (context) => SearchResultsPage()),
+                              );
+                            },
+
+                            splashColor: Theme.of(context).primaryColor,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget>[
+                                Text(
+                                  'Find your room now!',
+                                  style: TextStyle(
+                                    fontSize: 25.0,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ],
+                            ),
                           ),
                         ),
                       ],
