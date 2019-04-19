5f44d17df3ed5fbfd87a61ac48517b43ef95dd77
Connor Harris <c.harris99@hotmail.co.uk>
2019-04-19 20:35:07 +0100

    Now takes form input from FindARoomFragment. Next integrate algorithm and new database results.

--- lib/pages/search_results_page.dart
+++ lib/pages/search_results_page.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 
 class SearchResultsPage extends StatelessWidget {
+  ///List of Strings to store the current and destination rooms from form in FindARoom fragment
+  final List<String> formRooms;
 
-  final backgroundImage = new Container(
+  ///Constructor
+  SearchResultsPage({Key key, @required this.formRooms}) : super(key: key);
+
+  ///Method to create background image
+  final _backgroundImage = new Container(
     height: 250.00,
     decoration: new BoxDecoration(
       image: new DecorationImage(
@@ -12,20 +18,22 @@ class SearchResultsPage extends StatelessWidget {
     ),
   );
 
-  final onImageContent = new Container(
+  ///Method to display title on Image
+  final _onImageContent = new Container(
 
   );
 
   @override
   Widget build(BuildContext context) {
-    return new Container(
+    ///@todo change to Scaffold 
+    return Container(
       color: Colors.white,
       padding: EdgeInsets.all(8.0),
       height: 250.0,
       child: new Stack(
         children: <Widget>[
-          backgroundImage,
-          onImageContent,
+          _backgroundImage,
+          _onImageContent,
         ],
       ),
     );
