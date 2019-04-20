bac727f30b922e8ecee974b652fc0fc7a4e088ad
Connor Harris <c.harris99@hotmail.co.uk>
2019-04-20 16:15:49 +0100

    Changed button and image design to fit spec

--- lib/fragments/find_a_room_fragment.dart
+++ lib/fragments/find_a_room_fragment.dart
@@ -6,71 +6,69 @@ import 'package:csc2022_app/managers/find_a_room_manager.dart';
 
 class FindARoomFragment extends StatelessWidget {
 
+  ///Method to build button
+  Widget _buildButton(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(left: 15.0, top: 35.0, right: 15.0, bottom: 35.0),
+      child: MaterialButton(
+        height: 75.0,
+        minWidth: 125.0,
+        color: Theme.of(context).primaryColor,
+        textColor: Colors.white,
+
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => SearchFormPage()),
+          );
+        },
+
+        splashColor: Theme
+            .of(context)
+            .primaryColor,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Icon(
+              Icons.search,
+              size: 45.0,
+              color: Colors.black
+            ),
+            Text(
+              'Search for a room',
+              style: TextStyle(
+                fontSize: 30.0,
+                color: Colors.white,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  ///Method to build map content
+  Widget _buildMapBody(BuildContext context) {
+    return Container(
+      height: 450.0,
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+            alignment: Alignment(-.2, 0),
+            image: AssetImage('assets/images/lecture_theatre.jpg'),
+            fit: BoxFit.cover),
+      ),
+    );
+  }
+
+
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
-        children: <Widget>[
-           DecoratedBox(
-            decoration: BoxDecoration( color: Color(0xFFFFFF)),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Stack(
-                  children: <Widget>[
-                    Image.asset('assets/images/lecture_theatre.jpg'),
-                    Container(
-                      margin: EdgeInsets.all(50.0),
-                      padding: EdgeInsets.only(left: 10.0, top: 10.0, right: 10.0, bottom: 10.0),
-                      color: Color(0xFF313131),
-                      child: Text(
-                        'Search for a room!',
-                        //@todo Add center wrapper back and chenage image + remove decorated box and text colour
-                        style: TextStyle(
-                          color: Theme.of(context).primaryColor,
-                          fontSize: 27.0,
-                        ),
-                        textAlign: TextAlign.center,
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-           ),
-
-           Padding(
-             padding: EdgeInsets.all(15.0),
-             child: MaterialButton(
-               height: 75.0,
-               minWidth: 125.0,
-               color: Colors.black54,
-               textColor: Colors.white,
-
-               onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => SearchFormPage()),
-                  );
-                },
-
-               splashColor: Theme.of(context).primaryColor,
-               child: Row(
-                 mainAxisAlignment: MainAxisAlignment.center,
-                 children: <Widget>[
-                   Text(
-                     'Find your room now!',
-                     style: TextStyle(
-                       fontSize: 25.0,
-                       color: Colors.white,
-                     ),
-                   ),
-                 ],
-               ),
-             ),
-           ),
-        ],
-      ),
+    return Column(
+      children: <Widget>[
+        ///@todo replace image with map when finished
+        _buildMapBody(context),
+        _buildButton(context)
+      ]
     );
   }
 }
