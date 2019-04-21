b31f8e0c03b59cf0ee0ec5fe8382437ee9650191
Mason Powell <mj.powell@hotmail.com>
2019-04-21 23:34:32 +0100

    Added getGraph method to navigation_manager

--- lib/managers/navigation_manager.dart
+++ lib/managers/navigation_manager.dart
@@ -5,21 +5,12 @@ import 'package:csc2022_app/helpers/database_helper.dart';
 import 'package:csc2022_app/algorithm/node.dart';
 import 'package:csc2022_app/algorithm/graph.dart';
 
-class Floor {
-  //Stores info for each floor
-  const Floor(this.name, this.path, this.floorNumber);
-
-  final String name;
-  final String path;
-  final int floorNumber;
-}
-
 class NavigationManager {
 
   static Map<String, Node> nodes;
 
-  /// Returns all the notable [Node]s for a given floor.
-  static Future<Map<String, Node>> getRooms(int floor) async {
+  /// Returns all the [Node]s for a given floor.
+  static Future<Map<String, Node>> getNodes(int floor) async {
     // Execute query to get required database rows.
     List<Map<String, dynamic>> queryResults = await DatabaseHelper.query(
         'SELECT Name, XCoord, YCoord FROM Room WHERE ID LIKE $floor%'
@@ -27,10 +18,35 @@ class NavigationManager {
 
     // Create a [Node] for all rows returned from the DB query.
     for (Map<String, dynamic> m in queryResults) {
-      nodes[m['ID']] = Node.fromDB(m['Name'],m['XCoord'],m['YCoord']);
+      nodes[m['ID']] = Node.fromDB(m['Name'], m['XCoord'], m['YCoord']);
     }
 
     return nodes;
   }
+
+  static Future<Graph> getGraph(int floor) async {
+    Graph graph = Graph.floor(floor);
+    String f;
+
+    if (floor == 0) {
+      f = 'g';
+    }
+
+    // Execute query to get required database rows.
+    List<Map<String, dynamic>> queryResults = await DatabaseHelper.query(
+        'SELECT Room 1 ID, Room 2 ID, Weight FROM Edge WHERE Room 1 ID LIKE $f%'
+    );
+
+    for (Map<String, dynamic> m in queryResults) {
+      nodes[m['Room 1 ID']].addDestination(nodes[m['Room 2 ID']], m['Weight']);
+    }
+
+    nodes.forEach((key, value) {
+      graph.addNode(value);
+    });
+
+    return graph;
+  }
+
 
 }
