f60b732b4a593a619b343439f9543e5d8de93d09
Connor Harris <c.harris99@hotmail.co.uk>
2019-04-21 16:18:22 +0100

    Added and resized map placeholder. Added destination and origin ListTile

--- lib/pages/search_results_page.dart
+++ lib/pages/search_results_page.dart
@@ -7,35 +7,49 @@ class SearchResultsPage extends StatelessWidget {
   ///Constructor
   SearchResultsPage({Key key, @required this.formRooms}) : super(key: key);
 
-  ///Method to create background image
-  final _backgroundImage = new Container(
-    height: 250.00,
-    decoration: new BoxDecoration(
-      image: new DecorationImage(
-          image: AssetImage('assets/images/lecture_theatre.jpg'),
-          fit: BoxFit.cover,
+  ///Method to build map content
+  Widget _buildMapBody(BuildContext context) {
+    return Container(
+      height: 350.0,
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+            alignment: Alignment(-.2, 0),
+            image: AssetImage('assets/images/lecture_theatre.jpg'),
+            fit: BoxFit.cover),
       ),
-    ),
-  );
+    );
+  }
 
-  ///Method to display title on Image
-  final _onImageContent = new Container(
-
-  );
+  ///Method to build [ListTiles] to display route instructions.
+  //Widget _buildInstructionDisplay(BuildContext context) {
+    //return
+  //}
 
   @override
   Widget build(BuildContext context) {
-    ///@todo change to Scaffold 
-    return Container(
-      color: Colors.white,
-      padding: EdgeInsets.all(8.0),
-      height: 250.0,
-      child: new Stack(
-        children: <Widget>[
-          _backgroundImage,
-          _onImageContent,
-        ],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Find a room"),
       ),
+      body: Column(
+        children: <Widget>[
+          ///@todo replace image with map when finished
+          _buildMapBody(context),
+
+          Expanded(
+            //ListView
+            child: ListView(
+              children: <Widget>[
+                //Destination [ListTile]
+                ListTile(
+                  title: Text(formRooms[0], textAlign: TextAlign.start,),
+                  subtitle: Text('From ' + formRooms[1], textAlign: TextAlign.start,),
+                ),
+              ],
+            ),
+          ),
+        ]
+      )
     );
   }
 
