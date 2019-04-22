4f5f38b628af82b98c967deae1cd0afd2cd97067
Connor Harris <c.harris99@hotmail.co.uk>
2019-04-22 14:40:17 +0100

    Improved dropdown for new db. Attempting to integrate navigation

--- lib/managers/find_a_room_manager.dart
+++ lib/managers/find_a_room_manager.dart
@@ -5,7 +5,11 @@ import 'package:csc2022_app/helpers/database_helper.dart';
 import 'package:csc2022_app/managers/explore_a_floor_manager.dart';
 import 'package:csc2022_app/algorithm/navigation.dart';
 import 'package:csc2022_app/algorithm/node.dart';
+import 'package:csc2022_app/algorithm/graph.dart';
+import 'package:csc2022_app/managers/navigation_manager.dart';
 import 'dart:collection';
+import 'package:flutter/foundation.dart';
+import 'dart:developer';
 
 class FindARoomManager {
 
@@ -46,32 +50,87 @@ class FindARoomManager {
   static Future<List<String>> getRoomSuggestions(String pattern) async {
     // Execute query, return rows
     List<Map<String, dynamic>> queryResults = await DatabaseHelper.query(
-        'SELECT * FROM rooms WHERE name LIKE \'$pattern%\''
+        'SELECT ID FROM Room WHERE ID LIKE \'$pattern%\''
     );
     List<String> rooms = <String>[];
     // Create a [Room] for a ll rows returned from the DB query.
     for (Map<String, dynamic> m in queryResults) {
       rooms.add(
-          Room(m['name'], m['image']).name
+           m['ID']
       );
     }
     return rooms;
   }
 
+  /// Easy user interface method to take rooms as [List] of [String]s
+  /// and return directions as [List] of [String]s
+  static Future<List<String>> getDirections(List<String> formRooms) async {
+    Future<Queue<Node>> directionsQueue = getDirectionsQueue(formRooms);
+    Future<List<String>> directionsList = getDirectionDetails(directionsQueue);
+
+    return directionsList;
+  }
+
+  /// Return [Queue] of [Node]s of order between rooms given a [List] of [String]s containing
+  /// the current room name and destination room name
+  static Future<Queue<Node>> getDirectionsQueue(List<String> rooms) async {
+    // Store startRoom floor as an integer
+    int startFloor;
+    if(rooms[0].substring(0, 1) == "G") {
+      startFloor = 0;
+    } else {
+      startFloor = int.parse(rooms[0].substring(0, 1));
+    }
+    // Build Graph for floor of start node
+    Graph startGraph = await NavigationManager.getGraph(startFloor);
+
+    Node source;
+    Node destination;
+
+    // Store rooms as source and target nodes
+    for (Node w in startGraph.nodes) {
+      if (w.name == rooms[0]) {
+        source = w;
+
+        break;
+      }
+    }
+
+    for (Node v in startGraph.nodes) {
+      if (v.name == rooms[1]) {
+
+        destination = v;
+        break;
+      }
+    }
+
+    // Store directions from algorithm
+    Queue<Node> directions = Navigation.pathToTarget(startGraph, source, destination);
+
+    String s = directions.toString();
+    debugPrint('s: $s');
+    log('s: $s');
+
+    return directions;
+  }
+  
   /// Return [List] of [String]s storing directions given a queue of [Node]s
-  static Future<List<String>> getDirectionDetails(Queue<Node> nodeQueue) async {
+  static Future<List<String>> getDirectionDetails(Future<Queue<Node>> nodeQueue) async {
+    Queue<Node> nodeQueue1 = await nodeQueue;
     //List to store directions
     List<String> directions = new List<String>();
 
     //Iterate over all but last node
-    for(int i = 0; i < nodeQueue.length -1; i++) {
+    for(int i = 0; i < nodeQueue1.length; i++) {
       //Store name of node and if not null next node
-      String destA = nodeQueue.removeLast().name;
-      String destB = nodeQueue.removeLast().name;
+      String destA = nodeQueue1.removeLast().name;
+      String destB = nodeQueue1.removeLast().name;
+      debugPrint('dd: $destA');
+      debugPrint('db: $destB');
 
       //Query edge table for edge between the first and second nodes
       List<Map<String, dynamic>> queryResults = await DatabaseHelper.query(
-          "SELECT `A to B` FROM `Edge` where `Room 1 ID` = " + destA + " AND `Room 2 ID` = " + destB
+          "SELECT `A to B` FROM Edge where `Room 1 ID` = " + destA + " AND `Room 2 ID` = " + destB
       );
 
       //For results, add to directions list
--- lib/pages/search_results_page.dart
+++ lib/pages/search_results_page.dart
@@ -1,11 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:csc2022_app/managers/find_a_room_manager.dart';
+import 'dart:collection';
+import 'package:flutter/foundation.dart';
+import 'dart:developer';
 
-class SearchResultsPage extends StatelessWidget {
+class SearchResultsPage extends StatefulWidget {
   ///List of Strings to store the current and destination rooms from form in FindARoom fragment
   final List<String> formRooms;
 
+  @override
+  State<StatefulWidget> createState() {
+    return _SearchResultsState();
+  }
+
   ///Constructor
   SearchResultsPage({Key key, @required this.formRooms}) : super(key: key);
+}
+class _SearchResultsState extends State<SearchResultsPage> {
+
+  ///List of [String]s to store the route directions
+  Future<List<String>> _directions() async {
+    List<String> dir = await FindARoomManager.getDirections(widget.formRooms);
+    return dir;
+  }
+
+  ///Method to build ListTiles as directions
+  Widget _createDirectionTiles(BuildContext context) {
+    FutureBuilder<List<String>>(
+      future: _directions(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          // Store directions
+          List<String> values = snapshot.data;
+          return ListView.builder(
+            itemCount: values.length,
+            itemBuilder: (BuildContext context, int index) {
+              return Column(
+                children: <Widget>[
+                  ListTile(
+                    title: Text(values[index]),
+                  ),
+                  Divider(height: 2.0,),
+                ],
+              );
+            },
+          );
+        } else {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+      },
+    );
+  }
 
   ///Method to build map content
   Widget _buildMapBody(BuildContext context) {
@@ -27,6 +74,7 @@ class SearchResultsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    log('LOG');
     return Scaffold(
       appBar: AppBar(
         title: Text("Find a room"),
@@ -36,20 +84,54 @@ class SearchResultsPage extends StatelessWidget {
           ///@todo replace image with map when finished
           _buildMapBody(context),
 
-          Expanded(
+          /*Expanded(
             //ListView
             child: ListView(
               children: <Widget>[
                 //Destination [ListTile]
                 ListTile(
-                  title: Text(formRooms[0], textAlign: TextAlign.start,),
-                  subtitle: Text('From ' + formRooms[1], textAlign: TextAlign.start,),
+                  title: Text(widget.formRooms[0], textAlign: TextAlign.start,),
+                  subtitle: Text('From ' + widget.formRooms[1], textAlign: TextAlign.start,),
                 ),
               ],
             ),
+          ),*/
+          FutureBuilder(
+            future: _directions(),
+            builder: (BuildContext context, AsyncSnapshot snapshot) {
+              switch (snapshot.connectionState) {
+                case ConnectionState.none:
+                case ConnectionState.waiting:
+                  return new Center(
+                    child: CircularProgressIndicator()
+                  );
+                default:
+                  if (snapshot.hasError)
+                    return new Text('Error: ${snapshot.error}');
+                  else
+                    return createListView(context, snapshot);
+              }
+            },
           ),
         ]
       )
+    );
+  }
+
+  Widget createListView(BuildContext context, AsyncSnapshot snapshot) {
+    List<String> values = snapshot.data;
+    return new ListView.builder(
+      itemCount: values.length,
+      itemBuilder: (BuildContext context, int index) {
+        return new Column(
+          children: <Widget>[
+            new ListTile(
+              title: new Text(values[index]),
+            ),
+            new Divider(height: 2.0,),
+          ],
+        );
+      },
     );
   }
 
