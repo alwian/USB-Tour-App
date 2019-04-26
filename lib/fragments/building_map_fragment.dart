0949a3aa0e1d9dd86c321f649317cc07910769d9
Mason Powell <mj.powell@hotmail.com>
2019-04-26 14:22:12 +0100

    Initial implementation of the page and features

--- lib/fragments/building_map_fragment.dart
+++ lib/fragments/building_map_fragment.dart
@@ -10,7 +10,7 @@ import 'package:csc2022_app/algorithm/navigation.dart';
 import 'package:csc2022_app/managers/navigation_manager.dart';
 
 class Floor {
-  //Stores info for each floor
+  //Stores info for each floor.
   Floor(this.name, this.path, this.floorNumber);
 
   final String name;
@@ -39,7 +39,6 @@ class _BuildingMapState extends State<BuildingMapFragment> {
   Node _source;
   Node _target;
   _Route route;
-  //Graph graph;
 
   Queue<Node> path = new Queue<Node>();
   List<List<Node>> floorNodes = new List<List<Node>>();
@@ -56,10 +55,11 @@ class _BuildingMapState extends State<BuildingMapFragment> {
     }
     _loadElements(floorNodes, floors);
 
-    //initialisation for the floor selection
+    //Initialisation for the floor selection.
     selectedFloor = floor0;
     dropdownValue = floor0;
     floorNum = 0;
+
   }
 
   Future<void> _loadNodes(int floor, List<Node> floorList) async {
@@ -87,205 +87,233 @@ class _BuildingMapState extends State<BuildingMapFragment> {
 
   @override
   Widget build(BuildContext context) {
-    if (sourceListOpen == true) {
-      return ListView.builder(
-          padding: const EdgeInsets.all(10.0),
-          itemCount: floorNodes[floorNum]
-              .length, //list of all the nodes on the specified floor (floorNum)
-          itemBuilder: (context, i) {
-            return new ListTile(
-                title: Text(floorNodes[floorNum][i]
-                    .name), //the name of node i in list floorNum
-                onTap: () {
-                  if (floorNodes[floorNum][i] == _target) {
-                    Scaffold.of(context).showSnackBar(new SnackBar(
-                      content:
-                          new Text("Can't have matching source and target"),
-                    ));
-                  } else {
-                    setState(() {
-                      _source = floorNodes[floorNum][i];
-                      sourceListOpen = false; //closes list
-                    });
-                  }
-                });
-          });
-    } else if (targetListOpen == true) {
-      return ListView.builder(
-          padding: const EdgeInsets.all(10.0),
-          itemCount: floorNodes[floorNum]
-              .length, //list of all the nodes on the specified floor (floorNum)
-          itemBuilder: (context, i) {
-            return new ListTile(
-                title: Text(floorNodes[floorNum][i]
-                    .name), //the name of node i in list floorNum
-                onTap: () {
-                  if (floorNodes[floorNum][i] == _source) {
-                    Scaffold.of(context).showSnackBar(new SnackBar(
-                      content:
-                          new Text("Can't have matching source and target"),
-                    ));
-                  } else {
-                    setState(() {
-                      _target = floorNodes[floorNum][i];
-                      targetListOpen = false; //closes list
-                    });
-                  }
-                });
-          });
-    } else {
-      if (path.isNotEmpty) {
-        return Scaffold(
-          body: Column(children: <Widget>[
-            DropdownButton<Floor>(
-              value: dropdownValue,
-              onChanged: (Floor newValue) {
-                setState(() {
-                  dropdownValue = newValue;
-                  selectedFloor = newValue;
-                  floorNum = newValue.floorNumber;
+    if (path.isNotEmpty) {
+      return Scaffold(
+        body: Column(children: <Widget>[
+          DropdownButton<Floor>(
+            value: dropdownValue,
+            onChanged: (Floor newValue) {
+              setState(() {
+                dropdownValue = newValue;
+                selectedFloor = newValue;
+                floorNum = newValue.floorNumber;
 
-                  path = new Queue<Node>(); //empties the path
-                });
-              },
-              items: floors.map<DropdownMenuItem<Floor>>((Floor value) {
-                return DropdownMenuItem<Floor>(
-                  value: value,
-                  child: Text(value.name),
-                );
-              }).toList(),
-            ),
-            Expanded(
-              child: Container(
-                child: PhotoView.customChild(
-                  child: new CustomPaint(
-                      foregroundPainter: RoutePainter(path),
-                      child: Image(image: AssetImage(selectedFloor.path))),
-                  minScale: PhotoViewComputedScale.contained,
-                  maxScale: 1.5,
-                  childSize: Size(4961,
-                      3508), //the height of the currently used images, need changing from magic numbers
-                  backgroundDecoration: new BoxDecoration(
-                    color: Colors.white,
-                  ),
+                path = new Queue<Node>(); //Empties the path.
+              });
+            },
+            items: floors.map<DropdownMenuItem<Floor>>((Floor value) {
+              return DropdownMenuItem<Floor>(
+                value: value,
+                child: Text(value.name),
+              );
+            }).toList(),
+          ),
+          Expanded(
+            child: Container(
+              child: PhotoView.customChild(
+                child: new CustomPaint(
+                    foregroundPainter: RoutePainter(path),
+                    child: Image(image: AssetImage(selectedFloor.path))),
+                minScale: PhotoViewComputedScale.contained,
+                maxScale: 1.5,
+                //The height of the currently used images, need changing from magic numbers.
+                childSize: Size(4961, 3508),
+                backgroundDecoration: new BoxDecoration(
+                  color: Colors.white,
                 ),
               ),
-            )
-          ]),
-          bottomNavigationBar: BottomAppBar(
-              child: new Row(
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              IconButton(
-                icon: Icon(Icons.trip_origin),
-                onPressed: () {
-                  setState(() {
-                    sourceListOpen = true;
-                  });
-                },
-              ),
-              IconButton(
-                icon: Icon(Icons.lens),
-                onPressed: () {
-                  setState(() {
-                    targetListOpen = true;
-                  });
-                },
-              ),
-            ],
-          )),
-          floatingActionButton: FloatingActionButton(
-            onPressed: () {
-              setState(() {
+            ),
+          )
+        ]),
+        bottomNavigationBar: BottomAppBar(
+            child: new Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            IconButton(
+              icon: Icon(Icons.trip_origin),
+              onPressed: () {
+                setState(() {
+                  _navigateAndDisplayRoomList(context, 1);
+                });
+              },
+            ),
+            IconButton(
+              icon: Icon(Icons.lens),
+              onPressed: () {
+                setState(() {
+                  _navigateAndDisplayRoomList(context, 2);
+                });
+              },
+            ),
+          ],
+        )),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            setState(() {
+              if (_source == null || _target == null) {
+                Scaffold.of(context).showSnackBar(new SnackBar(
+                  content:
+                      new Text("Both source and target must be selected first"),
+                ));
+              } else {
                 path = _Route(_source, _target, floorNum, selectedFloor.graph)
                     .generateRoute();
+              }
+            });
+          },
+          tooltip: 'draw route',
+          child: Icon(Icons.near_me),
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      );
+    } else {
+      return Scaffold(
+        body: Column(children: <Widget>[
+          DropdownButton<Floor>(
+            value: dropdownValue,
+            onChanged: (Floor newValue) {
+              setState(() {
+                dropdownValue = newValue;
+                selectedFloor = newValue;
+                floorNum = newValue.floorNumber;
+                path = new Queue<Node>(); //empties the path
               });
             },
-            tooltip: 'draw route',
-            child: Icon(Icons.near_me),
+            items: floors.map<DropdownMenuItem<Floor>>((Floor value) {
+              return DropdownMenuItem<Floor>(
+                value: value,
+                child: Text(value.name),
+              );
+            }).toList(),
           ),
-          floatingActionButtonLocation:
-              FloatingActionButtonLocation.centerDocked,
-        );
-      } else {
-        return Scaffold(
-          body: Column(children: <Widget>[
-            DropdownButton<Floor>(
-              value: dropdownValue,
-              onChanged: (Floor newValue) {
-                setState(() {
-                  dropdownValue = newValue;
-                  selectedFloor = newValue;
-                  floorNum = newValue.floorNumber;
-                  path = new Queue<Node>(); //empties the path
-                });
-              },
-              items: floors.map<DropdownMenuItem<Floor>>((Floor value) {
-                return DropdownMenuItem<Floor>(
-                  value: value,
-                  child: Text(value.name),
-                );
-              }).toList(),
-            ),
-            Expanded(
-              child: Container(
-                child: PhotoView(
-                  imageProvider: AssetImage(selectedFloor.path),
-                  minScale: PhotoViewComputedScale.contained,
-                  maxScale: 1.5,
-                  backgroundDecoration: new BoxDecoration(
-                    color: Colors.white,
-                  ),
+          Expanded(
+            child: Container(
+              child: PhotoView(
+                imageProvider: AssetImage(selectedFloor.path),
+                minScale: PhotoViewComputedScale.contained,
+                maxScale: 1.5,
+                backgroundDecoration: new BoxDecoration(
+                  color: Colors.white,
                 ),
               ),
             ),
-          ]),
-          bottomNavigationBar: BottomAppBar(
-              child: new Row(
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              IconButton(
-                icon: Icon(Icons.trip_origin),
-                onPressed: () {
-                  setState(() {
-                    sourceListOpen = true;
-                  });
-                },
-              ),
-              IconButton(
-                icon: Icon(Icons.lens),
-                onPressed: () {
-                  setState(() {
-                    targetListOpen = true;
-                  });
-                },
-              ),
-            ],
-          )),
-          floatingActionButton: FloatingActionButton(
-            onPressed: () {
-              setState(() {
-                if (_source == null || _target == null) {
-                  Scaffold.of(context).showSnackBar(new SnackBar(
-                    content: new Text(
-                        "Both source and target must be selected first"),
-                  ));
-                } else {
-                  path = _Route(_source, _target, floorNum, selectedFloor.graph)
-                      .generateRoute();
-                }
-              });
-            },
-            tooltip: 'draw route',
-            child: Icon(Icons.near_me),
           ),
-          floatingActionButtonLocation:
-              FloatingActionButtonLocation.centerDocked,
-        );
-      }
+        ]),
+        bottomNavigationBar: BottomAppBar(
+            child: new Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            IconButton(
+              icon: Icon(Icons.trip_origin),
+              onPressed: () {
+                setState(() {
+                  _navigateAndDisplayRoomList(context, 1);
+                });
+              },
+            ),
+            IconButton(
+              icon: Icon(Icons.lens),
+              onPressed: () {
+                setState(() {
+                  _navigateAndDisplayRoomList(context, 2);
+                });
+              },
+            ),
+          ],
+        )),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            setState(() {
+              if (_source == null || _target == null) {
+                Scaffold.of(context).showSnackBar(new SnackBar(
+                  content:
+                      new Text("Both source and target must be selected first"),
+                ));
+              } else {
+                path = _Route(_source, _target, floorNum, selectedFloor.graph)
+                    .generateRoute();
+              }
+            });
+          },
+          tooltip: 'draw route',
+          child: Icon(Icons.near_me),
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      );
     }
+  }
+
+  _navigateAndDisplayRoomList(BuildContext context, int sot) async {
+    // returns a future that will complete after we call
+    // Navigator.pop on RoomList.
+    if (sot == 1) {
+      _source = await Navigator.push(
+          context,
+          MaterialPageRoute(
+              builder: (context) =>
+                  RoomList(sot, floorNum, _source, _target, floorNodes)));
+    } else if (sot == 2) {
+      _target = await Navigator.push(
+          context,
+          MaterialPageRoute(
+              builder: (context) =>
+                  RoomList(sot, floorNum, _source, _target, floorNodes)));
+    }
+  }
+}
+
+class RoomList extends StatelessWidget {
+  int sot;
+  int floorNum;
+  Node _source;
+  Node _target;
+  List<List<Node>> floorNodes;
+
+  RoomList(@override this.sot, @override this.floorNum, @override this._source,
+      @override this._target, @override this.floorNodes);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Select a room'),
+        ),
+        body: ListView.builder(
+            padding: const EdgeInsets.all(10.0),
+            itemCount: floorNodes[floorNum].length,
+            //List of all the nodes on the specified floor (floorNum).
+            itemBuilder: (context, i) {
+              return new ListTile(
+                  //The name of node i in list floorNum.
+                  title: Text(floorNodes[floorNum][i].name),
+                  onTap: () {
+                    if (sot == 1) {
+                      //1 means we are setting the source.
+                      if (floorNodes[floorNum][i] == _target) {
+                        Scaffold.of(context).showSnackBar(new SnackBar(
+                          content:
+                              new Text("Can't have matching source and target"),
+                        ));
+                      } else {
+                        _source = floorNodes[floorNum][i];
+                        Navigator.pop(context, _source);
+                      }
+                    } else if (sot == 2) {
+                      //2 means we are setting the target.
+                      if (floorNodes[floorNum][i] == _source) {
+                        Scaffold.of(context).showSnackBar(new SnackBar(
+                          content:
+                              new Text("Can't have matching source and target"),
+                        ));
+                      } else {
+                        _target = floorNodes[floorNum][i];
+                        Navigator.pop(context, _target);
+                      }
+                    }
+                  });
+            }));
   }
 }
 
@@ -335,56 +363,8 @@ class _Route {
   Graph currentGraph;
   Queue<Node> route;
 
-  //these nodes would already be in the database, but for now they're just local for testing
-  /*Node a = new Node("a");
-  Node b = new Node("b");
-  Node c = new Node("c");
-  Node d = new Node("d");
-  Node e = new Node("e");
-  Node f = new Node("f");
-  Node g = new Node("g");*/
-
   _Route(Node source, Node target, int floor, Graph graph) {
     currentGraph = graph;
-    //pre-initialisation test code start
-    /*a.addDestination(b, 10);
-    a.addDestination(c, 15);
-    a.coordsX = 3112;
-    a.coordsY = 1176;
-
-    b.addDestination(d, 12);
-    b.addDestination(f, 15);
-    b.coordsX = 1812;
-    b.coordsY = 1137;
-
-    c.addDestination(e, 10);
-    c.coordsX = 1455;
-    c.coordsY = 1065;
-
-    d.addDestination(e, 2);
-    d.addDestination(f, 1);
-    d.coordsX = 1377;
-    d.coordsY = 1399;
-
-    f.addDestination(e, 5);
-    f.addDestination(g, 5);
-    f.coordsX = 1377;
-    f.coordsY = 2800;
-
-    e.coordsX = 602;
-    e.coordsY = 3100;
-
-    g.coordsX = 964;
-    g.coordsY = 2872;
-
-    graph.addNode(a);
-    graph.addNode(b);
-    graph.addNode(c);
-    graph.addNode(d);
-    graph.addNode(e);
-    graph.addNode(f);
-    graph.addNode(g);*/
-    //end
 
     this.source = source;
     this.target = target;
