6cbcaf5c1a2cfffe257c6db8b90d911b6d62c301
Connor Harris <c.harris99@hotmail.co.uk>
2019-04-27 21:09:28 +0100

    Added test group for getting directions as a list

--- test/find_a_room_test.dart
+++ test/find_a_room_test.dart
@@ -11,7 +11,7 @@ import 'package:sqflite/sqflite.dart';
 /// Main method to run tests
 void main() {
 
-  // Test for retrieving all room ids from database
+  /// Test for retrieving all room ids from database
   group('Getting all Rooms data', () {
     //Test not empty
     test('Getting room data', () async {
@@ -29,7 +29,7 @@ void main() {
   // Known specific valid suggestion
   String specificSuggestion = "1.002";
 
-  // Test for getting room suggestions
+  /// Test for getting room suggestions
   group('Getting room ID suggestions', () {
     //Test valid suggestions return
     test('Using valid suggestion', () async {
@@ -54,7 +54,7 @@ void main() {
     });
   });
 
-  // Test for getting directions as a [Queue] of [Node]s
+  /// Test for getting directions as a [Queue] of [Node]s
   group('Getting route as a Queue of Nodes', () {
     // Known invalid input
     List<String> invalidEqual = ['Q', 'Q'];
@@ -69,9 +69,9 @@ void main() {
     List<String> validInput = ['1.002', '1.006'];
 
     // Nodes in expected output
-    Node one = new Node('1.002');
+    Node one = new Node('1.006');
     Node two = new Node('1.005');
-    Node three = new Node('1.006');
+    Node three = new Node('1.002');
 
     // Expected returned Queue
     Queue<Node> expectedOutput;
@@ -82,14 +82,65 @@ void main() {
     // Test valid input room ids as input
     test('Using valid room input', () async {
       Queue<Node> route = await FindARoomManager.getDirectionsQueue(validInput);
-      expect(route, expectedOutput);
+
+      // Check strings for actual and expected outputs for equality,
+      // as actual has many more generated details
+      for(int i = 0; i < route.length; i++) {
+        expect(route.removeFirst().toString(), expectedOutput.removeFirst().toString());
+      }
     });
   });
 
-  // Test for getting List of directions
-  group('', () {
+  // Test for getting [List] of directions
+  group('Getting list of suggestions', () {
+    // Nodes in known valid input
+    Node one = new Node('1.006');
+    Node two = new Node('1.005');
+    Node three = new Node('1.002');
 
+    // Valid input Queue
+    Queue<Node> validInput;
+    validInput.add(one);
+    validInput.add(two);
+    validInput.add(three);
+
+    // Expected output from given valid input
+    List<String> expectedFromValid = ['Facing the front of the lecture theatre, go to the bottom of the stairs and go round the left corner.',
+    'With the window of your left, walk through the double doors in front of you.'];
+
+    // Test valid input
+    test('Using valid input', () async {
+      List<String> actualOutput = await FindARoomManager.getDirectionDetails(validInput);
+
+      // Iterate over [actualOutput] and compare values to [expectedFromValid]
+      for(int i = 0; i < actualOutput.length; i++) {
+        expect(actualOutput[i], expectedFromValid[i]);
+      }
+    });
+
+    // Nodes in known invalid input
+    Node invalidOne = new Node('G.062A');
+    Node invalidTwo = new Node('G.062');
+
+    // Invalid input [Queue]
+    Queue<Node> invalidInput;
+    invalidInput.add(invalidOne);
+    invalidInput.add(invalidTwo);
+    
+    // Expected output from invalid inputs
+    List<String> expectedFromInvalid = ['No route could be found'];
+    
+    // Test using invalid input
+    test('Using invalid input', () async {
+      List<String> actualOutput = await FindARoomManager.getDirectionDetails(
+          invalidInput);
+
+      expect(actualOutput[0], expectedFromInvalid[0]);
+    });
   });
+  
+  /// Integration test on getDirections method
+  group('', body)
 
 
 }
