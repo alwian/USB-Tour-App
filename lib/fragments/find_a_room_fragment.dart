30c9d7e53929c0119a517af0670871405cc46d72
Connor Harris <c.harris99@hotmail.co.uk>
2019-04-27 11:02:11 +0100

    Added floor map carousel

--- lib/fragments/find_a_room_fragment.dart
+++ lib/fragments/find_a_room_fragment.dart
@@ -3,8 +3,18 @@ import 'package:csc2022_app/pages/search_results_page.dart';
 import 'package:csc2022_app/pages/search_form_page.dart';
 import 'package:flutter_typeahead/flutter_typeahead.dart';
 import 'package:csc2022_app/managers/find_a_room_manager.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 class FindARoomFragment extends StatelessWidget {
+
+  //List of images to display in Carousel
+  final List<String> imageList = [
+    "assets/images/floor0.png",
+    "assets/images/floor1.png",
+    "assets/images/floor2.png",
+    "assets/images/floor3.png",
+    "assets/images/floor4.png"
+  ];
 
   ///Method to build button
   Widget _buildButton(BuildContext context) {
@@ -49,14 +59,31 @@ class FindARoomFragment extends StatelessWidget {
 
   ///Method to build map content
   Widget _buildMapBody(BuildContext context) {
-    return Container(
+
+    return CarouselSlider(
       height: (MediaQuery.of(context).size.height) * 0.66,
-      decoration: const BoxDecoration(
-        image: DecorationImage(
-            alignment: Alignment(-.2, 0),
-            image: AssetImage('assets/images/lecture_theatre.jpg'),
-            fit: BoxFit.cover),
-      ),
+      autoPlayInterval: Duration(seconds: 5),
+      autoPlayAnimationDuration: Duration(seconds: 1),
+      autoPlay: true,
+      pauseAutoPlayOnTouch: Duration(seconds: 7),
+      viewportFraction: 0.9,
+      aspectRatio: 1.0,
+      items: imageList.map(
+            (url) {
+          return Container(
+            child: ClipRRect(
+              borderRadius: BorderRadius.all(Radius.circular(0.0)),
+              child: Image.asset(
+                url,
+                fit: BoxFit.cover,
+                width: MediaQuery.of(context).size.width,
+                height: (MediaQuery.of(context).size.height),
+                alignment: Alignment(-.2, 0),
+              ),
+            ),
+          );
+        },
+      ).toList(),
     );
   }
 
