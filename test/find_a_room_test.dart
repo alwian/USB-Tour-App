2e0b353381243acf2c73013a60c760e50f1e4676
Connor Harris <c.harris99@hotmail.co.uk>
2019-04-27 19:44:32 +0100

    Added test group for getting directoins as a queue

--- test/find_a_room_test.dart
+++ test/find_a_room_test.dart
@@ -1,5 +1,6 @@
 import 'package:test/test.dart';
 import 'package:csc2022_app/managers/find_a_room_manager.dart';
+import 'package:csc2022_app/algorithm/node.dart';
 import 'dart:collection';
 import 'package:sqflite/sqflite.dart';
 
@@ -25,6 +26,9 @@ void main() {
   // Known invalid input
   String invalidSuggestion = 'L.99';
 
+  // Known specific valid suggestion
+  String specificSuggestion = "1.002";
+
   // Test for getting room suggestions
   group('Getting room ID suggestions', () {
     //Test valid suggestions return
@@ -38,6 +42,53 @@ void main() {
       List<String> suggestions = await FindARoomManager.getRoomSuggestions(invalidSuggestion);
       expect(suggestions, isEmpty);
     });
+
+
+    // Expected output
+    List<String> expected = ['1.002'];
+
+    //Test valid suggestions return for specific id
+    test('Using specific valid suggestion', () async {
+      List<String> suggestions = await FindARoomManager.getRoomSuggestions(specificSuggestion);
+      expect(suggestions, expected);
+    });
+  });
+
+  // Test for getting directions as a [Queue] of [Node]s
+  group('Getting route as a Queue of Nodes', () {
+    // Known invalid input
+    List<String> invalidEqual = ['Q', 'Q'];
+
+    // Test invalid room ids as input
+    test('Using invalid room input', () async {
+      Queue<Node> route = await FindARoomManager.getDirectionsQueue(invalidEqual);
+      expect(route, isEmpty);
+    });
+
+    // Known valid input
+    List<String> validInput = ['1.002', '1.006'];
+
+    // Nodes in expected output
+    Node one = new Node('1.002');
+    Node two = new Node('1.005');
+    Node three = new Node('1.006');
+
+    // Expected returned Queue
+    Queue<Node> expectedOutput;
+    expectedOutput.add(one);
+    expectedOutput.add(two);
+    expectedOutput.add(three);
+
+    // Test valid input room ids as input
+    test('Using valid room input', () async {
+      Queue<Node> route = await FindARoomManager.getDirectionsQueue(validInput);
+      expect(route, expectedOutput);
+    });
+  });
+
+  // Test for getting List of directions
+  group('', () {
+
   });
 
 
